e63a173090d03079746a07b93fd68a69bd355130
JohnKamaujk <johnjkkamau@gmail.com>
2024-07-05 18:37:46 +0300

    :sparkles:feat:PG connection established

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
+require("dotenv").config(); // Add this line at the very top
+
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors"); 
-
+const cors = require("cors");
+const sequelize = require("./utils/database"); // Import the database connection
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.use(cors());
 app.use(bodyParser.json());
 
-
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log(
+      "Connection to the database has been established successfully."
+    );
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 
 app.listen(PORT, () => {
--- src/utils/database.js
+++ src/utils/database.js
@@ -3,7 +3,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(
   process.env.PG_DB,
   process.env.PG_USER,
-  process.env.PG_PASSWORD,
+  process.env.PG_PASSWORD, 
   {
     host: process.env.PG_HOST,
     dialect: "postgres",
