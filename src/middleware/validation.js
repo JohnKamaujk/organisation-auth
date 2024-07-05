f420e9f23985a8e7dcce07165ed9b799d387d16b
JohnKamaujk <johnjkkamau@gmail.com>
2024-07-05 23:05:19 +0300

    :sparkles:feat:Validate Registration

---
+++ src/middleware/validation.js
@@ -0,0 +1,42 @@
+const { check, validationResult } = require("express-validator");
+
+const handleValidationErrors = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      errors: errors.array().map((error) => ({
+        field: error.param,
+        message: error.msg,
+      })),
+    });
+  }
+  next();
+};
+
+const registrationValidationRules = [
+  check("firstName")
+    .notEmpty()
+    .withMessage("First name is required")
+    .isLength({ min: 2 })
+    .withMessage("First name must be at least 2 characters long"),
+  check("lastName")
+    .notEmpty()
+    .withMessage("Last name is required")
+    .isLength({ min: 2 })
+    .withMessage("Last name must be at least 2 characters long"),
+  check("email").isEmail().withMessage("Invalid email format"),
+  check("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+];
+
+const validateRegistration = [
+  ...registrationValidationRules,
+  handleValidationErrors,
+];
+
+module.exports = {
+  validateRegistration,
+};
