8915b76caa5073571ede87d0c89fb41fc9f21945
JohnKamaujk <johnjkkamau@gmail.com>
2024-07-06 15:36:49 +0300

    :sparkles:feat:get user route

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,7 +6,11 @@ const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (token == null) return res.sendStatus(401);
+  if (token == null)
+    return res.status(401).json({
+      message: "not authorized",
+      statusCode: 401,
+    });
 
   jwt.verify(token, JWT_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
