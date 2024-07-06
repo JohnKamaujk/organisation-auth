8183bce23c1f7534125e925227927321bb9423c8
JohnKamaujk <johnjkkamau@gmail.com>
2024-07-06 20:05:28 +0300

    :sparkles:feat:create organisation

--- src/routes/organisationsRoutes.js
+++ src/routes/organisationsRoutes.js
@@ -5,11 +5,12 @@ const {
   createOrganisation,
 } = require("../controllers/organisationsController");
 const { authenticateToken } = require("../middleware/auth");
+const { validateOrganisation } = require("../middleware/validation");
 
 const router = express.Router();
 
 router.get("/", authenticateToken, getAllOrganisations);
 router.get("/:orgId", authenticateToken, getOrganisation);
-router.post("/", authenticateToken, createOrganisation);
+router.post("/", authenticateToken, validateOrganisation, createOrganisation);
 
 module.exports = router;
