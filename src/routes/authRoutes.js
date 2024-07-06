42a72ef697f744e94c024a0ed486d627cd801358
JohnKamaujk <johnjkkamau@gmail.com>
2024-07-06 13:00:37 +0300

    :sparkles:feat: login route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { registerUser } = require("../controllers/authController");
+const { registerUser, loginUser } = require("../controllers/authController");
 const { validateRegistration } = require("../middleware/validation");
 
 const router = express.Router();
 
 router.post("/register", validateRegistration, registerUser);
+router.post("/login", loginUser)
 
 module.exports = router;
