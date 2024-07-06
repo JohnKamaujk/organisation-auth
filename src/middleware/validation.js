af5e060256c5e308a16a116c655cbf9d2bbc63de
JohnKamaujk <johnjkkamau@gmail.com>
2024-07-06 12:46:49 +0300

    :sparkles:feat: Register route

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -1,31 +1,33 @@
-const { check, validationResult } = require("express-validator");
+const { body, validationResult } = require("express-validator");
 
 const handleValidationErrors = (req, res, next) => {
   const errors = validationResult(req);
+
   if (!errors.isEmpty()) {
-    return res.status(422).json({
-      errors: errors.array().map((error) => ({
-        field: error.param,
-        message: error.msg,
-      })),
-    });
+    const formattedErrors = errors.array().map((error) => ({
+      field: error.path,
+      message: error.msg,
+    }));
+
+    return res.status(422).json({ errors: formattedErrors });
   }
+
   next();
 };
 
 const registrationValidationRules = [
-  check("firstName")
+  body("firstName")
     .notEmpty()
     .withMessage("First name is required")
     .isLength({ min: 2 })
     .withMessage("First name must be at least 2 characters long"),
-  check("lastName")
+  body("lastName")
     .notEmpty()
     .withMessage("Last name is required")
     .isLength({ min: 2 })
     .withMessage("Last name must be at least 2 characters long"),
-  check("email").isEmail().withMessage("Invalid email format"),
-  check("password")
+  body("email").isEmail().withMessage("Invalid email format"),
+  body("password")
     .notEmpty()
     .withMessage("Password is required")
     .isLength({ min: 6 })
