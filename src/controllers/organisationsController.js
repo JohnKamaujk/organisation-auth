e6f7dbb0f94f61efaee4093ceb2f15def934d7f6
JohnKamaujk <johnjkkamau@gmail.com>
2024-07-06 20:29:57 +0300

    :sparkles:feat:add users to organisation controller

--- src/controllers/organisationsController.js
+++ src/controllers/organisationsController.js
@@ -1,4 +1,5 @@
 const { Op } = require("sequelize");
+const User = require("../models/User");
 const Organisation = require("../models/Organisation");
 const UserOrganisation = require("../models/UserOrganisation");
 
@@ -109,8 +110,76 @@ const createOrganisation = async (req, res) => {
   }
 };
 
+const addUserToOrganisation = async (req, res) => {
+  const { orgId } = req.params;
+  const { userId } = req.body;
+
+  const loggedInUserId = req.user;
+
+  try {
+    // Check if the organisation exists
+    const organisation = await Organisation.findOne({ where: { orgId } });
+    if (!organisation) {
+      return res.status(404).json({
+        status: "Bad Request",
+        message: "Organisation not found",
+        statusCode: 404,
+      });
+    }
+
+    // Check if the logged-in user belongs to the organisation
+    const userOrg = await UserOrganisation.findOne({
+      where: { userId: loggedInUserId, orgId },
+    });
+    if (!userOrg) {
+      return res.status(403).json({
+        status: "Forbidden",
+        message: "You do not have permission to add users to this organisation",
+        statusCode: 403,
+      });
+    }
+
+    // Check if the user to be added exists
+    const user = await User.findOne({ where: { userId } });
+    if (!user) {
+      return res.status(404).json({
+        status: "Bad Request",
+        message: "User not found",
+        statusCode: 404,
+      });
+    }
+
+    // Check if the user is already a member of the organisation
+    const userAlreadyInOrg = await UserOrganisation.findOne({
+      where: { userId, orgId },
+    });
+    if (userAlreadyInOrg) {
+      return res.status(400).json({
+        status: "Bad Request",
+        message: "User is already a member of this organisation",
+        statusCode: 400,
+      });
+    }
+
+    // Add the user to the organisation
+    await UserOrganisation.create({ userId, orgId });
+
+    res.status(200).json({
+      status: "success",
+      message: "User added to organisation successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Server error",
+      statusCode: 500,
+    });
+  }
+};
+
 module.exports = {
   getAllOrganisations,
   getOrganisation,
   createOrganisation,
+  addUserToOrganisation,
 };
