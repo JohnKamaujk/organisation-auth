1672f828202035a15009034494d831718f58a295
JohnKamaujk <johnjkkamau@gmail.com>
2024-07-08 15:18:06 +0300

    :sparkles:feat:postgres deployment config

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,13 +1,13 @@
-const Sequelize = require("sequelize");
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.PG_DB,
-  process.env.PG_USER,
-  process.env.PG_PASSWORD, 
-  {
-    host: process.env.PG_HOST,
-    dialect: "postgres",
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 module.exports = sequelize;
